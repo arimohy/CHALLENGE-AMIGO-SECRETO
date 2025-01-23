728c7ee1400c6e8153883d600a4b26fdaa5887b0
arimohy <arimohy@gmail.com>
2025-01-23 11:35:34 -0500

    funcionalidad de pagina

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+function agregarAmigo(){
+    let amigonuevo=document.getElementById("amigo").value.trim();
+    
+    if(isCorrectInput(amigonuevo)){
+        amigos.push(amigonuevo);
+        actualizarListaAmigos();
+        ClearAmigo();
+    }
+}
+
+function actualizarListaAmigos() {
+    const ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+    amigos.forEach((amigo) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        ul.appendChild(li);
+    });
+}
+
+
+function isCorrectInput(entrada){
+    if(!entrada){
+        alert("Por favor, inserte un nombre.")
+        return false;
+    }
+    if(amigos.includes(entrada)){
+        alert("El amigo ya existe no se volvio a agregar");
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function ClearAmigo(){
+    document.getElementById("amigo").value = '';
+}
+
+function validarListaAmigos(){
+    return (amigos.length>=1);
+}
+function sortearAmigo(){
+    if(validarListaAmigos()){
+        let nroramdon=Math.floor(Math.random()*(amigos.length));
+        let amigoSecreto=amigos[nroramdon];
+        document.getElementById("resultado").innerText =`Amigo secreto es: ${amigoSecreto}`;
+    }
+    else{
+        alert("No hay amigos para sortear")
+    }
+}
